96c8ee48dd184765b7ccd819d3f09fc0fb47ff44
RomanEKB93 <oma170@hotmail.com>
2022-02-18 22:20:57 +0300

    4.1. Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Roman Shevchenko");
         System.out.println("16.09.1993");
     }
