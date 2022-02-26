ebe885eeb7fe8539ab81299163c944ae54613b7d
RomanEKB93 <oma170@hotmail.com>
2022-02-26 12:50:44 +0300

    5.7. Ипотека [#156315 #269610]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,9 +3,8 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-        double residue = amount;
-        while (residue > 0) {
-            residue = residue + residue * (percent / 100) - salary;
+        while (amount > 0) {
+            amount = amount + amount * (percent / 100) - salary;
             year++;
         }
         return year;
